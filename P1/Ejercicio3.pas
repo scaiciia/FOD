46e5ba5deaaa0f6bc625c1bfe45549f1829d2a8c
Santiago Caiciia Massello <scaiciia@gmail.com>
2024-03-14 20:20:10 -0300

    add P1: Ejercicio3

---
+++ P1/Ejercicio3.pas
@@ -0,0 +1,19 @@
+
+Program Ejercicio3;
+
+Type 
+    empleado =   Record
+        ID:   Integer;
+        apellido:   String;
+        nombre:   String;
+        edad:   Integer;
+        DNI:   String;
+    End;
+
+    archivo =   file Of empleado;
+
+Var 
+
+Begin
+
+End;
